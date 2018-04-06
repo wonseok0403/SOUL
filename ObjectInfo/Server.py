59eec9e1e074ce697f7ad62612665e524b9e2481
wonseok <wonseok0403@naver.com>
2018-04-06 16:26:57 +0900

    Changed 4/6, 16:26

--- ObjectInfo/Server.py
+++ ObjectInfo/Server.py
@@ -10,3 +10,15 @@ class Server(object) :
         self.OWNER_ID = ""
         self.SERVER_OS = ""
         self.SERVER_NAME = ""
+
+    def __init__(slef, i, p, s, ip, pa, u, n, id, os, Na) :
+        self.ID = i
+        self.CONNECTION_PORT = p
+        self.CONNECTION_SORT = s
+        self.CONNECTION_IPADDRESS = ip
+        self.CONNECTION_PASSWORD = pa
+        self.CONNECTION_USERNAME = u
+        self.OWNER_NAME = n
+        self.OWNER_ID = id
+        self.SERVER_OS = os
+        self.SERVER_NAME = Na
--- System/Connector.py
+++ System/Connector.py
@@ -1,5 +1,8 @@
 from SystemLoader import SystemLoader
 import psycopg2
+import pandas.io.sql as pdsql
+import pandas
+import sqlalchemy
 
 
 class Connector(object) :
@@ -12,6 +15,8 @@ class Connector(object) :
         # o yes
         self.db = self.SystemLoader.DB
         self.admin = self.SystemLoader.Admin
+        self.ServerList = [[]]
+        self.conn_string = ""
 
         self.Connecting()
 
@@ -25,18 +30,29 @@ class Connector(object) :
         # PostgreSQL will be the best choice. But if you want to use other version,
         # please check program version.
         self.Connect_DB()
+        self.Connect_getServerDB()
 
         # Second, program will get server data from your local database.
 
+    def Connect_getServerDB(self) :
+        print("Log, (Connect_getServerDB), conn_string : ", self.conn_string)
+        conn = psycopg2.connect(self.conn_string)
+        the_frame = pdsql.read_sql_table("SERVERS", self.engine)
+        print( the_frame.values.tolist() )
+        self.ServerList = the_frame.values.tolist()
+        print(self.ServerList)
 
 
     def Connect_DB(self):
         print('connect db')
         if self.db.SORTS == 'psql' :
-            conn_string = "host="+self.db.HOST+" dbname="+self.db.NAME+" user="+self.db.USER+" password="+self.db.PW
-            print( conn_string )
-            conn = psycopg2.connect(conn_string)
-            cursor = conn.cursor()
+            self.conn_string = "host="+self.db.HOST+" dbname="+self.db.NAME+" user="+self.db.USER+" password="+self.db.PW
+            print( self.conn_string )
+            # dialect+driver://username:password@host:port/database
+            self.engine = sqlalchemy.create_engine("postgresql+psycopg2://" + self.db.USER.replace("'","") + ":" + self.db.PW.replace("'","")+"@" + self.db.HOST.replace("'","") + ":" + self.db.PORT.replace("'","")+ "/" + self.db.NAME.replace("'",""))
+            conn = psycopg2.connect(self.conn_string)
+
+
 
         else :
             print ( "Sorry, " + db.SORTS + " isn't supported yet.....")
